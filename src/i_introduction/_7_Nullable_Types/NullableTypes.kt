6e0a9e074e39b38d8afcce6283681e42412f424d
Sok Pomaranczowy <sok_pomaranczowy>
2016-11-13 15:04:11 +0100

    done I chapter of kotlin koans

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -22,10 +22,12 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
     references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
-fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
-) {
-    todoTask7(client, message, mailer)
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    client?: return
+    message?: return
+    client?.personalInfo ?: return
+    client?.personalInfo?.email ?: return
+    mailer.sendMessage(client.personalInfo.email,message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
